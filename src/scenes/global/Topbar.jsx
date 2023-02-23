d3b55327c55a173b8b63616c452b5676fdc05561
Michael David Vinc <tawie18102000@gmail.com>
2023-02-23 20:43:21 +0700

    first commit

---
+++ src/scenes/global/Topbar.jsx
@@ -0,0 +1,21 @@
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material";
+import LightModeOutlinedIcon  from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon  from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon  from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon  from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon  from "@mui/icons-material/PersonOutlined";
+import SearchIcon  from "@mui/icons-material/Search";
+
+
+const Topbar = () => {
+    const theme = useTheme();
+
+    
+
+    return <div>Topbar</div>
+}
+
+export default Topbar;
